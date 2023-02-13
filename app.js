054220542a48fad7a93e2ba3d0d9c2bb4b49628b
Abhishek Sengupta <chotuame@gmail.com>
2023-02-13 21:46:30 +0530

    final.

--- app.js
+++ app.js
@@ -33,11 +33,11 @@ app.post("/",function(req,res){
             const wind=weatherData.wind.speed;
             const icon=weatherData.weather[0].icon;
             const imgurl=" http://openweathermap.org/img/wn/"+icon+"@4x.png";
-            res.write("<h1>The temperature in " +cityName+" is " + temp +"</h1>");
-            res.write("<img src= "+ imgurl+" >");
-            res.write("<h2>weather is " + weather_des + "</h2>");
-            res.write("<h2>Feels like " + fl + "</h2>"); 
-            res.write("<h2>Max and Min temperature is " + max_temp + " and " + min_temp + "</h2>");
+            res.write("<h1 style='color:#4B56D2;text-align:center'>The temperature in " +cityName+" is " + temp +"</h1>");
+            res.write("<img src= "+ imgurl+" style='margin-left:43%'>");
+            res.write("<h2 style='color:#5DA7DB;text-align:center'>Weather is " + weather_des + "</h2>");
+            res.write("<h2 style='color:#5837D0;text-align:center'>Feels like " + fl + "</h2>"); 
+            res.write("<h2 style='color:#4B56D2;text-align:center'>Max and Min temperature is " + max_temp + " and " + min_temp + "</h2>");
         })
      })
 
